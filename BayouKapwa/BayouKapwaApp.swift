01425891200de4c224432d8801faa45c908e16fc
Anika Morris <amorris@octavebio.com>
2025-07-04 12:32:01 -0500

    Initial Commit

---
+++ BayouKapwa/BayouKapwaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BayouKapwaApp.swift
+//  BayouKapwa
+//
+//  Created by Anika Morris on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BayouKapwaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
