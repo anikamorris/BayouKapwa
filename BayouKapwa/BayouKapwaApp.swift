252a7cbf5758e3bd354cb3380694cc653fc251d1
Anika Morris <amorris@octavebio.com>
2025-08-01 13:18:16 -0500

    add firebase package, add auth manager

--- BayouKapwa/BayouKapwaApp.swift
+++ BayouKapwa/BayouKapwaApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct BayouKapwaApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             WelcomeView()
---
+++ BayouKapwa/Managers/AuthenticationMangager.swift
@@ -0,0 +1,52 @@
+//
+//  AuthenticationMangager.swift
+//  BayouKapwa
+//
+//  Created by Anika Morris on 8/1/25.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct LocalAuthDataResult {
+    let id: String
+    let name: String?
+    let email: String?
+    let photoUrl: String?
+
+    init(user: User) {
+        self.id = user.uid
+        self.name = user.displayName
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+}
+
+enum AuthError: Error {
+    case noCurrentUser
+}
+
+final class AuthenticationMangager {
+
+    static let shared = AuthenticationMangager()
+
+    private init() { }
+
+    private func getAuthenticatedUser() throws -> LocalAuthDataResult {
+        guard let user = Auth.auth().currentUser else {
+            throw AuthError.noCurrentUser
+        }
+
+        return LocalAuthDataResult(user: user)
+    }
+
+    private func createNewUser(email: String, password: String) async throws -> LocalAuthDataResult {
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return LocalAuthDataResult(user: authDataResult.user)
+    }
+
+    private func signOut() throws {
+        try Auth.auth().signOut()
+    }
+
+}
